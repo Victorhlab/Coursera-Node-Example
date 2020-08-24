daad83f73abc30cb49882123f22cddc5bd72d7b2
victorhlab <Victorhlab@gmail.com>
2020-08-24 11:45:34 +0200

    Creating callbacks. So the rectangle functions execute at a later point in time - Simulating ASync behavior of JS.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var rect = require('./rectangle');
-
+const rect = require('./rectangle');
 
 function solveRect(l, b) {
     console.log("Solving for rectangle for l = " +l+" and b = "+b)
 
-    if(l <=0 || b <=0)
-    {
-        console.log("Rectangle dimentions should be > 0");
-    }
-    else{
-        console.log("The area of the rect is " + rect.area(l,b));
-        console.log("The perimeter of the rect is " + rect.perimeter(l,b));
-    }
+                    /* Callback fn */
+    rect(l, b, (err, rectangle) => {
+        /*If the error object exists - there is an error */
+        if(err) {
+            console.log("ERROR: ", err.message);
+        } 
+        else {
+            console.log("Area is: " + rectangle.area())
+            console.log("Perimeter is: " + rectangle.perimeter())
+        }
+    });
+
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,27 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area      = (x,y) => (x*y);
+                        /* Callback is error and then the JS object */
+module.exports = (x, y, callback) => {
+    if(x <=0 || y <=0)
+    {
+        //simulate work done
+        setTimeout(()=>
+            /* callback inputs are: error and value */ 
+            callback(new Error("Rectangle dimensions should be > 0"), 
+                    null), 
+                    2000);
+        
+    }
+    else{
+        //simulate work done
+        setTimeout(()=>
+            /* callback inputs are: error(null) and value */ 
+            callback(null, 
+                    {
+                        /* JS object */
+                        perimeter:( ) => (2*(x+y)),
+                        area:( ) => (x*y)
+                    }), 
+                    2000);
+    }
+}
+
+
